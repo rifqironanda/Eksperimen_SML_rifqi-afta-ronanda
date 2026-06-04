b033e503944bbfe18b633a1df9627696532549a3
rifqironanda <rifqironanda@gmail.com>
2026-06-04 12:56:20 +0700

    Add preprocessing unit test

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,11 +1,16 @@
-# tests/test_preprocessing.py
+import sys
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(ROOT_DIR))
 
 from preprocessing.automate_rifqi import preprocess_data
+
 
 def test_preprocess_not_empty():
 
     df = preprocess_data(
-        "dataset_raw/telco.csv",
+        "dataset_raw/WA_Fn-UseC_-Telco-Customer-Churn.csv",
         "preprocessing/dataset_preprocessing/temp.csv"
     )
 
